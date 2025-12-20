b68d26ed4411f81658c185c4a89617a664c3aa74
SobhanSoqandi <sobhansoqandi@gmail.com>
2025-12-20 13:50:09 +0330

    start project npm i

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Start from './components/Start'
+
+function App() {
+
+  return (
+    <>
+
+      <div>
+        <Start />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Start.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Start() {
+  return (
+    <div className="bg-blue-500 p-4" >
+        
+        Start</div>
+  )
+}
+
+export default Start;
