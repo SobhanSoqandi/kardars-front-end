5b4477e96f11d7852317420d7a92645bb40c5056
SobhanSoqandi <sobhansoqandi@gmail.com>
2026-01-04 19:48:24 +0330

    student and company panel completed

--- src/pages/companyPenel/component/Position.jsx
+++ src/pages/companyPenel/component/Position.jsx
@@ -1,13 +1,28 @@
 import React from "react";
 
 export default function Position() {
+
+  const status = 0;
+
+  const statusStyle = [
+    {
+      label: " فعال  ",
+      className: "badge--style bg-green-600",
+    },
+    {
+      label: " غیر فعال ",
+      className: "badge--style bg-red-400",
+    },
+  ];
   return (
     <div className="flex justify-between items-center p-4">
       <div className="space-y-1">
-        <div className="font-semibold text-xl">کارآموزی مهندسی نرم‌افزار</div>
-        <div>3 درخواست جدید</div>
+        <div className="font-semibold text-balance xl:text-xl">کارآموزی مهندسی نرم‌افزار</div>
+        <div className="text-sm xl:text-lg" >3 درخواست جدید</div>
       </div>
-      <div>فعال</div>
+      <div className={`w-28 ${statusStyle[status]?.className}`}>
+        {statusStyle[status]?.label}
+      </div>
     </div>
   );
 }
